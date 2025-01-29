a3b87e0a65d1d7981326f0c5f7d610dfa7301fe7
Mzamor71 <100431992+Mzamor71@users.noreply.github.com>
2025-01-29 02:10:03 +0000

    Initial Commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,18 @@
+public class BankAccount{
+    public static void main(String[] args){
+        double balance = 2175.37;
 
+        System.out.println("---------------Bob's Checking Account--------------");
+        System.out.println("    Starting Balance: " + balance);
+        System.out.println("    Withdrawal of $" + 302.50);
+        balance -= 302.50;
+        System.out.println("    New Balance: " + balance);
+        System.out.println("    Deposit of $"+ 50.03);
+        balance += 50.03;
+        System.out.println("    New Balance: " + balance);
+        System.out.println("    Withdraw half of balance");
+        balance /= 2;
+        System.out.println("    New Balance: "+ balance);
+        System.out.println("    Deposit of $" + 20.00);
+    }
+}
