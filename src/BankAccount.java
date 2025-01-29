5a1e2e846fa0d2e8041076f22ea438c2a2e5ae34
Mzamor71 <100431992+Mzamor71@users.noreply.github.com>
2025-01-29 15:33:14 +0000

    Changed the final output value to show 2 decimal values

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,18 +1,34 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BankAccount{
     public static void main(String[] args){
         double balance = 2175.37;
 
         System.out.println("---------------Bob's Checking Account--------------");
-        System.out.println("    Starting Balance: " + balance);
+        System.out.println("    Starting Balance: $" + balance);
         System.out.println("    Withdrawal of $" + 302.50);
         balance -= 302.50;
-        System.out.println("    New Balance: " + balance);
+        System.out.println("    New Balance: $" + balance);
         System.out.println("    Deposit of $"+ 50.03);
         balance += 50.03;
-        System.out.println("    New Balance: " + balance);
+        System.out.println("    New Balance: $" + balance);
         System.out.println("    Withdraw half of balance");
         balance /= 2;
-        System.out.println("    New Balance: "+ balance);
+        System.out.println("    New Balance: $"+ balance);
         System.out.println("    Deposit of $" + 20.00);
+        balance += 20.00;
+        System.out.println("    New Balance: $" + balance);
+        System.out.println("    Withdrawal of $" + 1.00);
+        balance -= 1.00;
+        System.out.println("    New Balance: $" + balance);
+        System.out.println("    Deposit of Paycheck");
+        balance *= 2;
+        System.out.println("    Deposit of $" + 1.00);
+        balance += 1;
+        BigDecimal ru = new BigDecimal(balance);
+        ru = ru.setScale(2, RoundingMode.HALF_UP);
+        System.out.println("    New Balance: $" + ru);
+
     }
 }
